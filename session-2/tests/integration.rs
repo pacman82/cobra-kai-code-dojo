abda81f9e81b3ef869879559f1f1f4483ce5a734
Markus Klein <markus-klein@live.de>
2022-09-13 18:53:26 +0200

    session 2 - property testing

--- session-2/tests/integration.rs
+++ session-2/tests/integration.rs
@@ -23,3 +23,28 @@ fn should_cutoff_for_4_000_000() {
     // Assert
     assert_eq!(4613732, sum_even_fib);
 }
+
+#[test]
+fn should_be_identical_to_slow_and_simple_solution() {
+    for n in 0..=1000 {
+        assert_eq!(
+            sum_even_fib_slow_and_simple(n),
+            sum_even_fib(n)
+        )
+    }
+}
+
+fn sum_even_fib_slow_and_simple(cutoff: u64) -> u64 {
+    fn fib(n: u64) -> u64 {
+        match n {
+            0 => 1,
+            1 => 2,
+            _ => fib(n - 1) + fib(n - 2),
+        }
+    }
+    (0..cutoff)
+        .map(fib)
+        .filter(|n| n % 2 == 0)
+        .take_while(|&n| n <= cutoff)
+        .sum()
+}
