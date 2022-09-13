66322f80716d3f976e98668c6836c26c41cb5e7e
Markus Klein <markus-klein@live.de>
2022-09-13 19:00:39 +0200

    session 2 - iterations

--- session-2/src/lib.rs
+++ session-2/src/lib.rs
@@ -1,14 +1,15 @@
 pub fn sum_even_fib(cutoff: u64) -> u64 {
-    fn fib(n: u64) -> u64 {
-        match n {
-            0 => 1,
-            1 => 2,
-            _ => fib(n - 1) + fib(n - 2),
-        }
-    }
     (0..cutoff)
         .map(fib)
         .filter(|n| n % 2 == 0)
         .take_while(|&n| n <= cutoff)
         .sum()
+}
+
+fn fib(n: u64) -> u64 {
+    match n {
+        0 => 1,
+        1 => 2,
+        _ => fib(n - 1) + fib(n - 2),
+    }
 }
