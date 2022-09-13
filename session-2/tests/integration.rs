c06eda8350e7787ebdd499dae7f80192dc579180
Markus Klein <markus-klein@live.de>
2022-09-13 18:48:26 +0200

    session 2 - cutoff 4_000_000

--- session-2/tests/integration.rs
+++ session-2/tests/integration.rs
@@ -9,5 +9,17 @@ fn should_be_44_for_cutoff_50() {
     let sum_even_fib = sum_even_fib(cutoff);
 
     // Assert
-    assert_eq!(44u64, sum_even_fib);
+    assert_eq!(44, sum_even_fib);
+}
+
+#[test]
+fn should_cutoff_for_4_000_000() {
+    // Given
+    let cutoff = 4_000_000;
+
+    // When
+    let sum_even_fib = sum_even_fib(cutoff);
+
+    // Assert
+    assert_eq!(4613732, sum_even_fib);
 }
